7259df482c4bad568abbec33f4b81870faf11b4f
atama <atama@DESKTOP-257PO3I>
2024-12-19 14:46:21 +0300

    Add project files.

---
+++ Локальный репозиторий/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Локальный_репозиторий
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
